0f3cce19300edde117ea150c033993c6a9a68eef
Tuyenxngoc <tuyenn9448@gmail.com>
2024-01-02 17:34:03 +0700

    Add page to view order details

--- src/pages/Admin/ManageOrders/OrderForm.js
+++ src/pages/Admin/ManageOrders/OrderForm.js
@@ -22,6 +22,8 @@ import { faSave } from '@fortawesome/free-regular-svg-icons';
 import { faArrowLeft, faClipboard } from '@fortawesome/free-solid-svg-icons';
 import { toast } from 'react-toastify';
 import { getBillInfo, getBillsByCustomerId } from '~/services/billService';
+import Product from '~/components/Product';
+import MoneyDisplay from '~/components/MoneyDisplay';
 
 const { Dragger } = Upload;
 
@@ -44,33 +46,96 @@ function OrderForm() {
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [orderId]);
 
+    if (!data) {
+        return <></>
+    }
+
     return (
         <div className='container my-3'>
             <div className='row justify-content-center gy-3'>
                 <div className='col-10'>
                     <div className={cx('panel-wrapper')}>
-                        <div className={cx('panel-header')}>
-                            <div className={cx('panel-title')}>
-                                <FontAwesomeIcon icon={faClipboard} className='me-3' />
-                                Đã hủy
-                            </div>
-                        </div>
                         <div className='panel-content'>
-
+                            {data.status}
                         </div>
                     </div>
                 </div>
 
                 <div className='col-10'>
                     <div className={cx('panel-wrapper')}>
-                        <div className={cx('panel-header')}>
-                            <div className={cx('panel-title')}>
-                                <FontAwesomeIcon icon={faClipboard} className='me-3' />
-                                {JSON.stringify(data)}
-                            </div>
-                        </div>
                         <div className='panel-content'>
+                            Mã đơn hàng   {data.id}
+                            <br />
+                            Địa chỉ nhận hàng {data.customer.address}
+                            <br />
+                            Người nhận hàng {data.customer.name}
+                        </div>
+                    </div>
+                </div>
 
+                <div className='col-10'>
+                    <div className={cx('panel-wrapper')}>
+                        <div className='panel-content'>
+                            <table className="table align-middle">
+                                <thead className='table-light'>
+                                    <tr>
+                                        <th scope="col">STT</th>
+                                        <th scope="col">Sản phẩm</th>
+                                        <th scope="col">Đơn giá</th>
+                                        <th scope="col">Số lượng</th>
+                                        <th scope="col">Thành tiền</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {data.billDetail.map((item, index) => {
+                                        const { product, quantity } = item;
+                                        return (
+                                            <tr key={index}>
+                                                <th scope="row" align='center'>{index + 1}</th>
+                                                <td align='left'>
+                                                    <div className={cx('product-item')}>
+                                                        <div className={cx('product-image')}>
+                                                            <img src={product.featuredImage} alt='featuredImage' />
+                                                        </div>
+                                                        <div className='product-detail'>
+                                                            <div className={cx('product-name')}>{product.name}</div>
+                                                        </div>
+                                                    </div>
+                                                </td>
+                                                <td align='left'>
+                                                    <MoneyDisplay amount={product.price} />
+                                                </td>
+                                                <td align='left'>{quantity}</td>
+                                                <td align='right'>
+                                                    <MoneyDisplay amount={product.price} discountPercent={product.discount} />
+                                                </td>
+                                            </tr>
+                                        );
+                                    })}
+                                </tbody>
+                                <tfoot>
+                                    <tr className={cx('footer')}>
+                                        <td align='right' colSpan='3'>Tổng tiền sản phẩm</td>
+                                        <td align='right' colSpan='2'>
+                                            <MoneyDisplay amount={data.total} />
+                                        </td>
+                                    </tr>
+                                    <tr className={cx('footer')}>
+                                        <td align='right' colSpan='3'>Phí vận chuyển</td>
+                                        <td align='right' colSpan='2'>
+                                            <MoneyDisplay amount={data.feeShip} />
+                                        </td>
+                                    </tr>
+                                    <tr className={cx('footer')}>
+                                        <td align='right' colSpan='3'>Doanh Thu Đơn Hàng</td>
+                                        <td align='right' colSpan='2'>
+                                            <span className={cx('total-price')}>
+                                                <MoneyDisplay amount={data.total - data.feeShip} />
+                                            </span>
+                                        </td>
+                                    </tr>
+                                </tfoot>
+                            </table>
                         </div>
                     </div>
                 </div>
